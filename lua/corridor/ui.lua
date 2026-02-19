4784c302667942d4c5933e1f58902ef33568e334
Orel <hi@orellazri.com>
2026-02-19 22:09:10 +0200

    support multi-line ghost text with virt_lines

--- lua/corridor/ui.lua
+++ lua/corridor/ui.lua
@@ -11,10 +11,25 @@ M.show = function(text)
 	M.clear()
 	M.current_suggestion = text
 
-	vim.api.nvim_buf_set_extmark(buf, ns_id, line, col, {
-		virt_text = { { text, "Comment" } },
+	-- Split suggestion into lines
+	local lines = vim.split(text, "\n", { plain = true })
+
+	-- Build the extmark options
+	local extmark_opts = {
+		virt_text = { { lines[1], "Comment" } },
 		virt_text_pos = "overlay",
-	})
+	}
+
+	-- If multi-line, add remaining lines as virt_lines below the current line
+	if #lines > 1 then
+		local virt_lines = {}
+		for i = 2, #lines do
+			table.insert(virt_lines, { { lines[i], "Comment" } })
+		end
+		extmark_opts.virt_lines = virt_lines
+	end
+
+	vim.api.nvim_buf_set_extmark(buf, ns_id, line, col, extmark_opts)
 end
 
 M.clear = function()
